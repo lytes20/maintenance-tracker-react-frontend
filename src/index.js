f7a8c09a5ebf105bb69fb6994357c0450da05c3d
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-09-24 18:10:54 +0300

    [Chore #160538782] Setup redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import './index.css';
-import MaintenanceTracker from './components/index';
+import MT from './containers/index';
 
-
-ReactDOM.render(<MaintenanceTracker />, document.getElementById('root'));
-
-
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <MT />
+  </Provider>,
+  document.getElementById('root')
+);
