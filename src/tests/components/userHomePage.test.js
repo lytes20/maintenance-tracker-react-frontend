f1015d61ecbc88e3704967cbfdca8c42a68754e5
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2018-10-10 17:16:27 +0300

    [Feature #160539438] Admin mark request as resolved

--- src/tests/components/userHomePage.test.js
+++ src/tests/components/userHomePage.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moxios from 'moxios';
+
 
 import UserHomePage from '../../components/userHomePage';
 
 let wrapper;
 
 beforeEach(() => {
+  moxios.install();
   wrapper = shallow(<UserHomePage />);
+});
+
+afterEach(() => {
+  moxios.uninstall();
 });
 
 describe('UserHomePage', () => {
